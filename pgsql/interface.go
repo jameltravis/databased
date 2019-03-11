5b393031571ab9ce0830d0145b71a88052ea0053
Jamel Travis <jameltravis@gmail.com>
2019-03-11 01:18:43 -0400

    Model interfaces taken straight from the mysql pkg

---
+++ pgsql/interface.go
@@ -0,0 +1,23 @@
+package pgsql
+
+import "database/sql"
+
+// IQuery is used for DB access. Each method corresponds to the associated
+// sql statement.
+type IQuery interface {
+	Select() (*sql.Rows, error)
+	Update() string
+	Insert() (int64, error)
+	Delete() (int64, error)
+}
+
+// IModel is an interface for models. Used so struct models are able to call
+// the methods below. To use this interface add Model as an embedded
+// field in a struct.
+type IModel interface {
+	IQuery
+	ListItems() (*sql.Rows, error)
+	LreateItem() (int64, error)
+	UpdateItem() string
+	DeleteItem() (int64, error)
+}
