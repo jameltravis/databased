eb860566641e4170ef4effea470df42d17ec3f34
Jamel Travis <jameltravis@gmail.com>
2019-03-10 22:59:42 -0400

    Removed model implementation to main function pkg

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -2,22 +2,12 @@ package mysql
 
 import "database/sql"
 
-//  List, Get, Create, Update, and Delete
-
-// APIActions are the standard actions used in resource oriented
-// design. This package assumes that you are adhering to this
-// design strategy.
-type APIActions interface {
+// IModel is an interface for models. Used so struct models are able to call
+// the methods below. To use this interface add Model as an embedded
+// field in a struct.
+type IModel interface {
 	listItems() (*sql.Rows, error)
 	createItem() (int64, error)
 	updateItem() string
 	deleteItem() (int64, error)
-}
-
-// Model forms the basis of normal models used by the user
-// package. Models should be used as an embeded type in
-// Models outside of this package.
-type Model struct {
-	db      *sql.DB
-	success bool
 }
